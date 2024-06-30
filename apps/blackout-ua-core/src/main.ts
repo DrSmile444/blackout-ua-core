3c5b4a6d60543080b0736d44a3bc948ef5956c11
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-06-30 20:11:12 +0300

    feat: add swagger description for core app

--- apps/blackout-ua-core/src/main.ts
+++ apps/blackout-ua-core/src/main.ts
@@ -6,7 +6,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('Outrage API').setDescription('The outrage API description').setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setTitle('Outrage Core Management API')
+    .setDescription(
+      `
+    The Outrage Management API serves as the core component for managing and filtering outrages,
+    as well as retrieving available cities and regions. This API enables clients to:
+
+    - Retrieve outrages, with capabilities to filter by various criteria.
+    - Manage and query the list of available cities and regions.
+    - Integrate with Telegram to parse and store messages from all regions.
+
+    The endpoints provided allow seamless interaction with the outrage data, ensuring
+    efficient data storage and retrieval mechanisms.
+  `,
+    )
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
