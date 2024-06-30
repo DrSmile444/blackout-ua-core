d5eb0dd2d6c6c2256a562bc7c61787cb9b9132d9
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-06-30 15:59:55 +0300

    style: add unicorn and ordered imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    './.eslint/ordered-imports.eslintrc.json5',
+    './.eslint/unicorn.eslintrc.json',
   ],
   root: true,
   env: {
