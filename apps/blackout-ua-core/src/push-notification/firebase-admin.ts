0a16b48183a1d82554b23248da03ec2b22df12cb
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-07-06 21:30:06 +0300

    feat: temporary mock firebase

--- apps/blackout-ua-core/src/push-notification/firebase-admin.ts
+++ apps/blackout-ua-core/src/push-notification/firebase-admin.ts
@@ -1,11 +1,19 @@
-import * as firebaseAdmin from 'firebase-admin';
+// import * as firebaseAdmin from 'firebase-admin';
 // import * as serviceAccount from './path/to/serviceAccountKey.json';
 // TODO add these credentials
-const serviceAccount = {};
+// const serviceAccount = {};
+//
+// firebaseAdmin.initializeApp({
+//   credential: firebaseAdmin.credential.cert(serviceAccount as firebaseAdmin.ServiceAccount),
+//   databaseURL: 'https://<your-database-name>.firebaseio.com',
+// });
 
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount as firebaseAdmin.ServiceAccount),
-  databaseURL: 'https://<your-database-name>.firebaseio.com',
-});
+import type { Message } from 'firebase-admin/lib/messaging/messaging-api';
+
+const firebaseAdmin = {
+  messaging: () => ({
+    send: (message: Message) => Promise.resolve('mocked'),
+  }),
+};
 
 export default firebaseAdmin;
