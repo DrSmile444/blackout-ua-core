125b603bb00e34dde528fc405eed9e18798ccac8
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2024-07-19 10:18:02 +0300

    feat: add custom port support for scrapper and telegram

--- apps/telegram/src/main.ts
+++ apps/telegram/src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -15,7 +16,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  const configService: ConfigService = app.get(ConfigService);
+  const port = configService.get<string>('TELEGRAM_PORT') || configService.get<string>('PORT') || 3000;
+
+  await app.listen(port);
+  console.info(`Telegram is running on port ${port}`);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
